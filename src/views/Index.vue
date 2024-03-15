3fff91a5fa93ab24e0b1c5397e085fcd42b98bf9
Nerdy Gedoni <gedoniani@gmail.com>
2024-03-15 15:54:57 +0100

    blog section

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -252,56 +252,6 @@
     </section>
     <!-- ===== Services End ===== -->
 
-    <!-- ===== Clients Start ===== -->
-    <section class="pj vp mr">
-        <!-- Section Title Start -->
-        <div>
-
-            <div class="animate_top bb ze rj ki xn vq">
-                <h2 class="fk vj pr kk wm on/5 gq/2 bb _b">
-                    Trusted by Global Brands
-                </h2>
-                <p class="bb on/5 wo/5 hq">
-                    `It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using.
-                </p>
-            </div>
-
-
-        </div>
-        <!-- Section Title End -->
-
-        <div class="bb ze ah ch pm hj xp ki xn 2xl:ud-px-49 bc">
-            <div class="wc rf qn zf cp kq xf wf">
-                <a href="#" class="rc animate_top">
-                    <img class="th wl ml il zl om" src="../assets/images/brand-light-01.svg" alt="Clients" />
-                    <img class="xc sk ml il zl nm" src="../assets/images/brand-dark-01.svg" alt="Clients" />
-                </a>
-                <a href="#" class="rc animate_top">
-                    <img class="tk ml il zl om" src="../assets/images/brand-light-02.svg" alt="Clients" />
-                    <img class="xc sk ml il zl nm" src="../assets/images/brand-dark-02.svg" alt="Clients" />
-                </a>
-                <a href="#" class="rc animate_top">
-                    <img class="tk ml il zl om" src="../assets/images/brand-light-03.svg" alt="Clients" />
-                    <img class="xc sk ml il zl nm" src="../assets/images/brand-dark-03.svg" alt="Clients" />
-                </a>
-                <a href="#" class="rc animate_top">
-                    <img class="tk ml il zl om" src="../assets/images/brand-light-04.svg" alt="Clients" />
-                    <img class="xc sk ml il zl nm" src="../assets/images/brand-dark-04.svg" alt="Clients" />
-                </a>
-                <a href="#" class="rc animate_top">
-                    <img class="tk ml il zl om" src="../assets/images/brand-light-05.svg" alt="Clients" />
-                    <img class="xc sk ml il zl nm" src="../assets/images/brand-dark-05.svg" alt="Clients" />
-                </a>
-                <a href="#" class="rc animate_top">
-                    <img class="tk ml il zl om" src="../assets/images/brand-light-06.svg" alt="Clients" />
-                    <img class="xc sk ml il zl nm" src="../assets/images/brand-dark-06.svg" alt="Clients" />
-                </a>
-            </div>
-        </div>
-    </section>
-    <!-- ===== Clients End ===== -->
-
     <!-- ===== Blog Start ===== -->
     <section class="ji gp uq">
         <!-- Section Title Start -->
@@ -311,8 +261,7 @@
                     Latest Blogs & News
                 </h2>
                 <p class="bb on/5 wo/5 hq">
-                    It is a long established fact that a reader will be distracted by the readable content of a page
-                    when looking at its layout. The point of using.
+                    Stay ahead of the curve with our insights into the latest trends shaping the event industry.
                 </p>
             </div>
 
