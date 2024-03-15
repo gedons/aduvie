f843c6c4811aeabf26a8f4805a90cbddf2555d3b
Nerdy Gedoni <gedoniani@gmail.com>
2024-03-15 13:10:12 +0100

    home sections

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -10,29 +10,60 @@
                 </a>
 
                 <!-- Hamburger Toggle BTN -->
-                <button class="po rc" @click="navigationOpen = !navigationOpen">
+                <button class="po rc" @click="toggleMenu">
                     <span class="rc i pf re pd">
                         <span class="du-block h q vd yc">
-                            <span class="rc i r s eh um tg te rd eb ml jl dl"
-                                :class="{ 'ue el': !navigationOpen }"></span>
-                            <span class="rc i r s eh um tg te rd eb ml jl fl"
-                                :class="{ 'ue qr': !navigationOpen }"></span>
-                            <span class="rc i r s eh um tg te rd eb ml jl gl"
-                                :class="{ 'ue hl': !navigationOpen }"></span>
+                            <span class="rc i r s eh um tg te rd eb ml jl dl" :class="{ 'ue el': menuOpen }"></span>
+                            <span class="rc i r s eh um tg te rd eb ml jl fl" :class="{ 'ue qr': menuOpen }"></span>
+                            <span class="rc i r s eh um tg te rd eb ml jl gl" :class="{ 'ue hl': menuOpen }"></span>
                         </span>
                         <span class="du-block h q vd yc lf">
-                            <span class="rc eh um tg ml jl el h na r ve yc"
-                                :class="{ 'sd dl': !navigationOpen }"></span>
-                            <span class="rc eh um tg ml jl qr h s pa vd rd"
-                                :class="{ 'sd rr': !navigationOpen }"></span>
+                            <span class="rc eh um tg ml jl el h na r ve yc" :class="{ 'sd dl': menuOpen }"></span>
+                            <span class="rc eh um tg ml jl qr h s pa vd rd" :class="{ 'sd rr': menuOpen }"></span>
                         </span>
                     </span>
                 </button>
                 <!-- Hamburger Toggle BTN -->
             </div>
 
-            <div class="vd wo/4 sd qo f ho oo wf yf" :class="{ 'd hh rm sr td ud qg ug jc yh': navigationOpen }">
+            <div class="vd wo/4 sd qo f ho oo wf yf" >
                 <nav>
+                    <ul class="tc _o sf yo cg ep">
+                        <li><a href="index.html" class="xl" :class="{ 'mk': page === 'home' }">Home</a></li>
+                        <li><a href="index.html#features" class="xl">Features</a></li>
+                        <li class="c i" x-data="{ dropdown: false }">
+                            <a href="#" class="xl tc wf yf bg" @click.prevent="dropdown = !dropdown"
+                                :class="{ 'mk': page === 'blog-grid' || page === 'blog-single' || page === 'signin' || page === 'signup' || page === '404' }">
+                                Pages
+
+                                <svg :class="{ 'wh': dropdown }" class="th mm we fd pf"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path
+                                        d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                                </svg>
+                            </a>
+
+                            <!-- Dropdown Start -->
+                            <ul class="a" :class="{ 'tc': dropdown }">
+                                <li><a href="blog-grid.html" class="xl" :class="{ 'mk': page === 'blog-grid' }">Blog
+                                        Grid</a></li>
+                                <li><a href="blog-single.html" class="xl" :class="{ 'mk': page === 'blog-single' }">Blog
+                                        Single</a></li>
+                                <li><a href="signin.html" class="xl" :class="{ 'mk': page === 'signin' }">Sign In</a>
+                                </li>
+                                <li><a href="signup.html" class="xl" :class="{ 'mk': page === 'signup' }">Sign Up</a>
+                                </li>
+                                <li><a href="404.html" class="xl" :class="{ 'mk': page === '404' }">404</a></li>
+                            </ul>
+                            <!-- Dropdown End -->
+                        </li>
+                        <li><a href="index.html#support" class="xl">Support</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="vd wo/4 sd qo f ho oo wf yf" :class="{ 'd hh rm sr td ud qg ug jc yh': !menuOpen }">
+                <nav v-if="!menuOpen">
                     <ul class="tc _o sf yo cg ep">
                         <li><a href="index.html" class="xl" :class="{ 'mk': page === 'home' }">Home</a></li>
                         <li><a href="index.html#features" class="xl">Features</a></li>
@@ -430,7 +461,8 @@
                     Let’s Stay Connected
                 </h2>
                 <p class="bb on/5 wo/5 hq">
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using.
+                    It is a long established fact that a reader will be distracted by the readable content of a page
+                    when looking at its layout. The point of using.
                 </p>
             </div>
 
@@ -759,3 +791,18 @@
     <!-- ===== Footer End ===== -->
 
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            menuOpen: true
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.menuOpen = !this.menuOpen;
+        }
+    }
+};
+</script>
