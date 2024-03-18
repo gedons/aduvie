ba8292cd4e58bb62977596efe19c12bb8b286f3f
Gedoni Ani <gedoniani@gmail.com>
2024-03-18 02:51:41 -0700

    admin api get all event

--- src/views/Admin/Events.vue
+++ src/views/Admin/Events.vue
@@ -285,119 +285,7 @@
 
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
 
-                    <div class="container px-6 py-8 mx-auto">
-                        <h3 class="text-3xl font-medium text-gray-700">Dashboard</h3>
-
-                        <div class="mt-4">
-                            <div class="flex flex-wrap -mx-6">
-                                <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
-                                    <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
-                                        <div class="p-3 bg-indigo-600 bg-opacity-75 rounded-full">
-                                            <svg class="w-8 h-8 text-white" viewBox="0 0 28 30" fill="none"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z"
-                                                    fill="currentColor"></path>
-                                            </svg>
-                                        </div>
-
-                                        <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">7</h4>
-                                            <div class="text-gray-500">New Users</div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
-                                    <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
-                                        <div class="p-3 bg-orange-600 bg-opacity-75 rounded-full">
-                                            <svg class="w-8 h-8 text-white" viewBox="0 0 28 28" fill="none"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M4.19999 1.4C3.4268 1.4 2.79999 2.02681 2.79999 2.8C2.79999 3.57319 3.4268 4.2 4.19999 4.2H5.9069L6.33468 5.91114C6.33917 5.93092 6.34409 5.95055 6.34941 5.97001L8.24953 13.5705L6.99992 14.8201C5.23602 16.584 6.48528 19.6 8.97981 19.6H21C21.7731 19.6 22.4 18.9732 22.4 18.2C22.4 17.4268 21.7731 16.8 21 16.8H8.97983L10.3798 15.4H19.6C20.1303 15.4 20.615 15.1004 20.8521 14.6261L25.0521 6.22609C25.2691 5.79212 25.246 5.27673 24.991 4.86398C24.7357 4.45123 24.2852 4.2 23.8 4.2H8.79308L8.35818 2.46044C8.20238 1.83722 7.64241 1.4 6.99999 1.4H4.19999Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M22.4 23.1C22.4 24.2598 21.4598 25.2 20.3 25.2C19.1403 25.2 18.2 24.2598 18.2 23.1C18.2 21.9402 19.1403 21 20.3 21C21.4598 21 22.4 21.9402 22.4 23.1Z"
-                                                    fill="currentColor"></path>
-                                                <path
-                                                    d="M9.1 25.2C10.2598 25.2 11.2 24.2598 11.2 23.1C11.2 21.9402 10.2598 21 9.1 21C7.9402 21 7 21.9402 7 23.1C7 24.2598 7.9402 25.2 9.1 25.2Z"
-                                                    fill="currentColor"></path>
-                                            </svg>
-                                        </div>
-
-                                        <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">6</h4>
-                                            <div class="text-gray-500">Total Orders</div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <!-- <div class="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
-                                    <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
-                                        <div class="p-3 bg-pink-600 bg-opacity-75 rounded-full">
-                                            <svg class="w-8 h-8 text-white" viewBox="0 0 28 28" fill="none"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor"
-                                                    stroke="currentColor" stroke-width="2" stroke-linejoin="round"></path>
-                                                <path
-                                                    d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z"
-                                                    stroke="currentColor" stroke-width="2"></path>
-                                            </svg>
-                                        </div>
-        
-                                        <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">{{ totalProducts }}</h4>
-                                            <div class="text-gray-500">Available Products</div>
-                                        </div>
-                                    </div>
-                                </div> -->
-
-                                <div class="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
-                                    <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
-                                        <div class="p-3 bg-green-600 bg-opacity-75 rounded-full">
-                                            <svg fill="#ffffff" class="w-8 h-8" viewBox="0 0 1024 1024"
-                                                xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
-                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
-                                                    stroke-linejoin="round"></g>
-                                                <g id="SVGRepo_iconCarrier">
-                                                    <path
-                                                        d="M277.675 981.521c5.657 0 10.24-4.583 10.24-10.24V499.514c0-5.651-4.588-10.24-10.24-10.24h-81.92c-5.652 0-10.24 4.589-10.24 10.24v471.767c0 5.657 4.583 10.24 10.24 10.24h81.92zm0 40.96h-81.92c-28.278 0-51.2-22.922-51.2-51.2V499.514c0-28.271 22.924-51.2 51.2-51.2h81.92c28.276 0 51.2 22.929 51.2 51.2v471.767c0 28.278-22.922 51.2-51.2 51.2zm275.456-40.96c5.657 0 10.24-4.583 10.24-10.24V408.777c0-5.657-4.583-10.24-10.24-10.24h-81.92a10.238 10.238 0 00-10.24 10.24v562.504c0 5.657 4.583 10.24 10.24 10.24h81.92zm0 40.96h-81.92c-28.278 0-51.2-22.922-51.2-51.2V408.777c0-28.278 22.922-51.2 51.2-51.2h81.92c28.278 0 51.2 22.922 51.2 51.2v562.504c0 28.278-22.922 51.2-51.2 51.2zm275.456-40.016c5.657 0 10.24-4.583 10.24-10.24V318.974c0-5.651-4.588-10.24-10.24-10.24h-81.92c-5.652 0-10.24 4.589-10.24 10.24v653.251c0 5.657 4.583 10.24 10.24 10.24h81.92zm0 40.96h-81.92c-28.278 0-51.2-22.922-51.2-51.2V318.974c0-28.271 22.924-51.2 51.2-51.2h81.92c28.276 0 51.2 22.929 51.2 51.2v653.251c0 28.278-22.922 51.2-51.2 51.2zM696.848 40.96l102.39.154c11.311.017 20.494-9.138 20.511-20.449S810.611.171 799.3.154L696.91 0c-11.311-.017-20.494 9.138-20.511 20.449s9.138 20.494 20.449 20.511z">
-                                                    </path>
-                                                    <path
-                                                        d="M778.789 20.571l-.307 101.827c-.034 11.311 9.107 20.508 20.418 20.542s20.508-9.107 20.542-20.418l.307-101.827C819.783 9.384 810.642.187 799.331.153s-20.508 9.107-20.542 20.418z">
-                                                    </path>
-                                                    <path
-                                                        d="M163.84 317.682h154.184a51.207 51.207 0 0036.211-14.999L457.208 199.71a10.263 10.263 0 017.237-3.003h159.754a51.235 51.235 0 0036.198-14.976l141.13-141.13c7.998-7.998 7.998-20.965 0-28.963s-20.965-7.998-28.963 0L631.447 152.755a10.265 10.265 0 01-7.248 2.992H464.445a51.226 51.226 0 00-36.201 14.999L325.271 273.719a10.244 10.244 0 01-7.248 3.003H163.839c-11.311 0-20.48 9.169-20.48 20.48s9.169 20.48 20.48 20.48z">
-                                                    </path>
-                                                </g>
-                                            </svg>
-                                        </div>
-
-                                        <div class="mx-5">
-                                            <h4 class="text-2xl font-semibold text-gray-700">5000</h4>
-                                            <div class="text-gray-500">Total Income</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
+                    <div class="container px-6 py-8 mx-auto">                    
                         <div class="mt-8">
                             <h3 class="text-3xl font-medium text-gray-700">Events</h3>
                         </div>
@@ -559,14 +447,25 @@ export default {
             dropdownOpen: false,
             selected: '',
             page: '',
-
+            events: [],
 
         };
     },
-
-
-
+    created() {       
+      this.fetchEvents();
+    },
     methods: {
+
+        fetchEvents() {
+              axios.get(`${api}/events/all`).then((response) => {
+              this.events = response.data.events;    
+              this.loading = false;       
+              })
+              .catch((error) => {
+              console.error('Error getting events:', error);   
+              this.loading = false;       
+              });      
+          },
 
         toggleSidebar() {
             this.sidebarOpen = !this.sidebarOpen;
